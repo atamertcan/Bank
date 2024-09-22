53436586676cb96b5f772b0c4a15c18d5603d45c
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-22 22:34:50 +0300

    Customer.java

--- Customer.java
+++ Customer.java
@@ -1,1 +1,13 @@
+import java.util.ArrayList;
 
+public class Customer {
+    
+    private String name;
+    private ArrayList<Double> transactions;
+    
+    public Customer(String name,double initialTransactions){
+        
+        this.name = name;
+        transactions = new ArrayList<>();
+        transactions.add(initialTransactions);
+    }
